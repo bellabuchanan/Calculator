8d9024431c58879d44d501af2c0d30a3af4f2bbf
Bella Buchanan <isabellabuchanan1@gmail.com>
2020-05-28 09:15:38 +0200

    clear and back buttons work now

--- script.js
+++ script.js
@@ -2,21 +2,42 @@ const display = document.querySelector(".display");
 
 const numbers = Array.from(document.querySelectorAll(".num"));
 
+let content = "";
 
+// add clicks for numbers and operators
 numbers.forEach(num => {
     num.addEventListener('click', function(e){
-        let nu = e.currentTarget.value
-        console.log(nu)
+        content += e.target.value;
+        display.textContent = content;
         
-        console.log('clicking')
+        console.log('clicking');
     });
 });
 
-
-const n = document.querySelector("#nine");
-
-n.addEventListener('click', function(e){
-    console.log(document.getElementById("nine").value)
-    console.log(e.currentTarget.value)
-    console.log('cluck');
+// clear all 
+const clear=document.querySelector(".clear");
+clear.addEventListener('click',function(e){
+    content="";
+    display.textContent=content;
 });
+
+// clear a character
+const back =document.querySelector(".back");
+back.addEventListener('click',function(e){
+    content = content.substring(0,content.length - 1);
+    display.textContent=content;
+});
+
+// operations
+function add(a,b){
+    return a+b
+};
+function subtract(a,b){
+    return a-b
+};
+function multiply(a,b){
+    return a*b
+};
+function divide(a,b){
+    return a/b
+};
